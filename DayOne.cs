1a0ecebf49ce2b9001ae267f5ce32445ccb93b71
William Cantin <william@hugocantin.info>
2020-12-01 15:57:13 +0000

    Added run time metrics

--- DayOne.cs
+++ DayOne.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections;
 using System.Linq;
+using System.Diagnostics;
 
 class DayOne: IRunDay {
+
   public void Run() {
+    Stopwatch stopwatch = Stopwatch.StartNew();
     Console.WriteLine ("--------- DayOne ---------");
     Part1();
+    stopwatch.Stop();
+    Console.WriteLine("Time elapsed: {0}", stopwatch.ElapsedMilliseconds);
     Console.WriteLine ("--------- DayOne Part2 ---------");
+    stopwatch = Stopwatch.StartNew();
     Part2();
+    stopwatch.Stop();
+    Console.WriteLine("Time elapsed: {0}", stopwatch.ElapsedMilliseconds);
   }
 
   private void Part1() {
@@ -17,7 +25,7 @@ class DayOne: IRunDay {
         foreach (var i2 in numbers) {
             var addition = i + i2;
             if(addition == 2020) {
-                Console.WriteLine($"Yes! {i} + {i2} == 2020");
+                // Console.WriteLine($"Yes! {i} + {i2} == 2020");
                 var result = i*i2;
                 Console.WriteLine($"Result: {result}");
             }
@@ -33,7 +41,7 @@ class DayOne: IRunDay {
             foreach (var i3 in numbers) {
                 var addition = i + i2 +i3;
                 if(addition == 2020) {
-                    Console.WriteLine($"Yes! {i} + {i2} + {i3} == 2020");
+                    // Console.WriteLine($"Yes! {i} + {i2} + {i3} == 2020");
                     var result = i*i2*i3;
                     Console.WriteLine($"Result: {result}");
                 }
